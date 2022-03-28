76b7df4df6d80eecee011e41e1670649c9ad81d5
Eric Lantz <ericmlantz@gmail.com>
2022-03-28 10:45:57 -0400

    Finished models/index.html to reference and export other models.

--- models/index.js
+++ models/index.js
@@ -1,3 +1,11 @@
 const {model} = require('mongoose')
 const RestaurantSchema = require('./Restaurant')
 const CustomerSchema = require('./Customer')
+
+const Restaurant = model('Restaurant', RestaurantSchema)
+const Customer = model('Customer', CustomerSchema)
+
+module.exports = {
+  Restaurant,
+  Customer
+}
