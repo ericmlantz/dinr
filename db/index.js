b73409c77208562caa097a9cc5082cd57c6ce303
Eric Lantz <ericmlantz@gmail.com>
2022-04-01 09:52:58 -0400

    Attempting Heroku Deployment

--- db/index.js
+++ db/index.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
+require('dotenv').config() // Add this line
+
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/dinrDatabase'
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/dinrDatabase')
+  .connect(dbUrl)
   .then(() => {
-    console.log('Successfully connected to MongoDB.')
+    console.log('Successfully connected to MongoDB!')
   })
-  .catch((err) => {     //e for error
-  console.error('Connection error', err.message)
+  .catch((e) => {
+    console.error('Connection error', e.message)
   })
-
-// Creates better debugging information for our queries if the following line is added
 mongoose.set('debug', true)
-
 const db = mongoose.connection
 
 module.exports = db
--- server.js
+++ server.js
@@ -15,8 +15,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'))
 app.use(express.static(`${__dirname}/client/build`))
 
+
+
 app.use('/', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+ })
+ 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
