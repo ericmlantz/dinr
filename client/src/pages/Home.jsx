85a1d61876613eff19a2430aa4eb9f487bbc2453
Eric Lantz <ericmlantz@gmail.com>
2022-04-01 09:24:21 -0400

    Css styling for homepage design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,10 @@
-import {useState} from 'react'
-import {Link} from 'react-router-dom'
-import axios from 'axios'
-
 const Home = () => {
 
   return (
     <div className="homepage">
-      <div className="top-page">
       <h1 className='title'>Dinr</h1>
         <p className="home-description">Description</p>
-      <img className='computer-img' src='https://i.imgur.com/pbkRh0V.png' alt="computer" width='50%'/>
-      </div>
-      <div className="bottom-page">
-      </div>
+      <img className='computer-img' src='https://i.imgur.com/HdjHzJy.png' alt="computer" width='50%'/>
     </div>
   )
 }
